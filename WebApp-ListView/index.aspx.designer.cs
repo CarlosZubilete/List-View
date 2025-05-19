4e2c267588ea1fa71b16116625abda381ba6d602
CZubileteSanchez <carloszubilete@gmail.com>
2025-05-19 01:46:20 -0300

    Created a index.css for the styles. Changed the table of the database, 'Peliculas' to 'Reparto'

--- WebApp-ListView/index.aspx.designer.cs
+++ WebApp-ListView/index.aspx.designer.cs
@@ -24,13 +24,13 @@ namespace WebApp_ListView
     protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
     /// <summary>
-    /// list control.
+    /// ListView1 control.
     /// </summary>
     /// <remarks>
     /// Auto-generated field.
     /// To modify move field declaration from designer file to code-behind file.
     /// </remarks>
-    protected global::System.Web.UI.WebControls.ListView list;
+    protected global::System.Web.UI.WebControls.ListView ListView1;
 
     /// <summary>
     /// sqlData_MaxiFlixDB control.
